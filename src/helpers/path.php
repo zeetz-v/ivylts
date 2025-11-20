931230873a7f3b0eb5700a5aa07fc62350f26d08
jesuszk <devjesus.br@outlook.com>
2025-11-20 16:08:22 -0300

    alterações: correção dos paths

--- src/helpers/path.php
+++ src/helpers/path.php
@@ -7,21 +7,27 @@ function path()
         {
             if ($path[0] === '/')
                 $path = mb_substr($path, 1);
-            return "./public/images/{$path}";
+            return $this->start_path() . "/public/images/{$path}";
         }
 
         function js(string $path)
         {
             if ($path[0] !== '/')
                 $path = "/{$path}";
-            return "./public/js{$path}";
+            return $this->start_path() . "/public/js{$path}";
         }
 
         function css(string $path)
         {
             if ($path[0] !== '/')
                 $path = "/{$path}";
-            return "./public/css{$path}";
+            return $this->start_path() . "/public/css{$path}";
+        }
+
+
+        function start_path()
+        {
+            return $_ENV["APP_ENVIRONMENT"] . '/' . str_replace("/var/www/htdocs/", "", getcwd());
         }
     };
 }
